d59e2b2c9eb11dffd76547cc8df9f37f157c58a6
koppresh <koppresh.putpak@agilepoint.com>
2022-04-22 15:43:52 +0530

    fix: added a safety check to for the /welcome route when the user is not logged in

--- src/modules/core-layout/pages/welcome-page.tsx
+++ src/modules/core-layout/pages/welcome-page.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { WelcomeHeader } from "modules/welcome-header/components/welcome-header";
 import { IssueWrapper } from "../components";
+import { LocalStorageKeys } from "common-enums";
+import { Link } from "react-router-dom";
 
 export const WelcomePage = React.memo(() => {
-
+    const isLoggedIn = localStorage.getItem(LocalStorageKeys.LOGIN_DATA)
     return (
         <>
-            <WelcomeHeader />
-            <IssueWrapper />
+            {
+                isLoggedIn ?
+                    <>
+                        <WelcomeHeader />
+                        <IssueWrapper />
+                    </>
+                    : <span>Please login to continue <Link to='/'>Click here to go back to login page</Link></span>
+            }
         </>
     )
 })
